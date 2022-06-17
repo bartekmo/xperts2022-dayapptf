991edd960ffaf9c08db35591867c9e2cae99ed3c
Bartek Moczulski <bmoczulski@gmail.com>
2022-06-17 08:08:11 +0200

    Update outputs.tf

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,3 @@
 output "service_ip" {
-  value = google_compute_instance.${var.prefix}-websrv.network_interface[0].access_config.nat_ip
+  value = google_compute_instance.websrv.network_interface[0].access_config.nat_ip
 }
